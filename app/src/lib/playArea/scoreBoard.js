63c82ed05c16005f9c45ad5d57d78337bbdedd24
DnMllr <yajnavalkya@cox.net>
2014-04-24 17:19:31 -0700

    Continued porting to transitionables, broke out functionality in the constructor.

--- app/src/lib/playArea/scoreBoard.js
+++ app/src/lib/playArea/scoreBoard.js
@@ -2,7 +2,7 @@ define(function(require) {
 
   var View             = require('famous/core/View');
   var Surface          = require('famous/core/Surface');
-  var StateModifier    = require('famous/modifiers/StateModifier');
+  var Modifier         = require('famous/core/Modifier');
   var Transform        = require('famous/core/Transform');
   var PlayerView       = require('./playerView');
   var Transitionable   = require('famous/transitions/Transitionable');
@@ -11,21 +11,26 @@ define(function(require) {
 
   //Helpers
 
-  function _init(s) {
-    s.add(s._modH).add(s._home);
-    s.add(s._modA).add(s._away);
-    s.add(s._modI).add(s._indicator);
+  function _init(scoreBoard) {
+    scoreBoard._modH.transformFrom(function() {
+      return Transform.translate(-1 * scoreBoard._translateX.get(), scoreBoard._translateY.get(), 0);
+    });
+    scoreBoard._modA.transformFrom(function() {
+      return Transform.translate(scoreBoard._translateX.get(), scoreBoard._translateY.get(), 0);
+    });
+    scoreBoard._modI.transformFrom(function() {
+      return Transform.translate(scoreBoard._translateI.get(), 80, 0);
+    });
+    scoreBoard._modI.opacityFrom(function() {
+      return (scoreBoard._translateY.get() + 30) / 30;
+    })
+    scoreBoard.add(scoreBoard._modH).add(scoreBoard._home);
+    scoreBoard.add(scoreBoard._modA).add(scoreBoard._away);
+    scoreBoard.add(scoreBoard._modI).add(scoreBoard._indicator);
   }
 
-  //Class
-
-  function ScoreBoard(url1, url2) {
-    View.call(this);
-    this._currentPlayer = 0;
-    this.position       = 'home';
-    this._home          = new PlayerView(url1, 0);
-    this._away          = new PlayerView(url2, 1);
-    this._indicator     = new Surface({
+  function createIndicator() {
+    return new Surface({
       size       : [true, true],
       content    : '<i class="icon-downarrow"></i>',
       classes    : ['indicator'],
@@ -35,23 +40,46 @@ define(function(require) {
         color     : 'rgb(0, 177, 253)'
       }
     });
-    this._modH = new StateModifier({
-      origin    : [0.5, 0],
-      transform : Transform.translate(-40, 0, 0)
+  }
+
+  function createMod() {
+    return new Modifier({
+      origin: [0.5, 0]
     });
-    this._modA = new StateModifier({
-      origin    : [0.5, 0],
-      transform : Transform.translate(40, 0, 0)
-    });
-    this._modI = new StateModifier({
-      origin: [0.5, 0],
-      transform: Transform.translate(-63, 80, 0)
-    });
-    this._spring = {
+  }
+
+  function _applyMods(scoreBoard) {
+    scoreBoard._modH = createMod();
+    scoreBoard._modA = createMod();
+    scoreBoard._modI = createMod();
+  }
+
+  function _applyTransitionables(scoreBoard) {
+    scoreBoard._translateX = new Transitionable(40);
+    scoreBoard._translateY = new Transitionable(0);
+    scoreBoard._translateI = new Transitionable(-63);
+  }
+
+  function _createPlayers(url1, url2) {
+    this._home = new PlayerView(url1, 0);
+    this._away = new PlayerView(url2, 1);
+  }
+
+  //Class
+
+  function ScoreBoard(url1, url2) {
+    View.call(this);
+    this._currentPlayer = 1;
+    this.position       = 'home';
+    this._indicator     = createIndicator();
+    this._spring        = {
       method       : 'spring',
       period       : 200,
       dampingRatio : 0.3
     };
+    _createPlayers.apply(this, arguments);
+    _applyTransitionables(this);
+    _applyMods(this);
     _init(this);
   }
 
@@ -60,30 +88,28 @@ define(function(require) {
 
   ScoreBoard.prototype.overlap = function() {
     this.position = 'overlap';
-    this._modH.halt();
-    this._modH.setTransform(Transform.translate(-20, -30, 0), this._spring);
-    this._modA.halt();
-    this._modA.setTransform(Transform.translate(20, -30, 0), this._spring);
-    this._modI.setOpacity(0, {curve: 'linear', duration: 100});
+    this._translateX.halt();
+    this._translateY.halt();
+    this._translateX.set(20, this._spring);
+    this._translateY.set(-30, this._spring);
   };
 
   ScoreBoard.prototype.home = function() {
     this.position = 'home';
-    this._modH.halt();
-    this._modH.setTransform(Transform.translate(-40, 0, 0), this._spring);
-    this._modA.halt();
-    this._modA.setTransform(Transform.translate(40, 0, 0), this._spring);
-    this._modI.setOpacity(1, {curve: 'linear', duration: 200});
+    this._translateX.halt();
+    this._translateY.halt();
+    this._translateX.set(40, this._spring);
+    this._translateY.set(0, this._spring);
   };
 
-  ScoreBoard.prototype.setPlayer = function(c) {
-    this._currentPlayer = c || this._currentPlayer ? 0 : 1;
-    this._modI.halt();
-    this._modI.setTransform(
-      Transform.translate(this._currentPlayer ? -40 : 40, 75, 0),
-      this._spring
-    );
-    if (this._currentPlayer) this._indicator.setProperties({
+  ScoreBoard.prototype.setPlayer = function(player) {
+    // player must be 1, home, or -1, away.
+    if (player !== 1 && player !== -1) this._currentPlayer = this._currentPlayer === -1 ? 1 : -1;
+    else this._currentPlayer = player;
+    var destination = this._currentPlayer === 1 ? -63 : 17;
+    this._translateI.halt();
+    this._translateI.set(destination, this._spring);
+    if (this._currentPlayer === 1) this._indicator.setProperties({
       color: 'rgb(0, 177, 253)'
     });
     else this._indicator.setProperties({
